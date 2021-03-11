626dc811bbb336f62e9635050cc0d83c3da58c3b
Jordi Villar <jordi.villar.bcn@gmail.com>
2021-03-11 18:06:40 +0100

    Exercici N1f3 rectificat. Afegida part omesa en la primera entrega

--- src/modul1/N1f3.java
+++ src/modul1/N1f3.java
@@ -3,13 +3,21 @@ package modul1;
 public class N1f3 {
 
 	public static void main(String[] args) {
-		final int ANY_INICIAL_TRASPAS = 1948;
+		int anyInicialTrespas = 1948;
 		final int PERIODE_TRASPAS = 4;
 		final int ANY_NEIXAMENT = 1968;
+		final String esAnyDeTrespas = "\nEl vostre any de neixament és any de trespàs";
+		final String noEsAnyDeTrespas = "\nEl vostre any de neixament no és any de trespàs";
+		boolean neixamentDeTrespas = ANY_NEIXAMENT%4 ==0;
 		
-		for (int any=ANY_INICIAL_TRASPAS; any <= ANY_NEIXAMENT; any+=PERIODE_TRASPAS) {
-			System.out.println(any);
+		System.out.println("Anys de trespàs fins a l'any de neixament:\n ");
+		for (int any=anyInicialTrespas; any <= ANY_NEIXAMENT; any+=PERIODE_TRASPAS) {
+			System.out.println("\t" + any);
+		}
+		if (neixamentDeTrespas == true) {
+			System.out.println(esAnyDeTrespas);
+		} else {
+			System.out.println(noEsAnyDeTrespas);
 		}
 	}
-
 }
